f7b8f3edf052bdc12f8afc88af00f2fe41a1dd8b
pc655 <pc8307@gmail.com>
2026-02-05 17:31:35 +0800

    Update workers.js

--- workers.js
+++ workers.js
@@ -4,7 +4,7 @@ const HTML_CONTENT = `
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Card Tab</title>
+    <title>我的导航</title>
     <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2280%22>⭐</text></svg>">
     <style>
     /* 全局样式 */
